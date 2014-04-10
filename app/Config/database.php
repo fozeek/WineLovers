719c28899a1418b6fa7039afae2ac60282a45698
nikosFram <nicolas.framery.2504@gmail.com>
2014-04-10 11:35:34 +0200

    tmp

--- app/Config/database.php
+++ app/Config/database.php
@@ -66,7 +66,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'root',
+		'password' => '',
 		'database' => 'winelovers',
 		'prefix' => '',
 		//'encoding' => 'utf8',
