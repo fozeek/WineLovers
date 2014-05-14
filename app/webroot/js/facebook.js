052047e5303735722c4118d00297a34259c21633
Muusha <bicheuxj@gmail.com>
2014-05-14 08:28:48 +0200

    Maj

--- app/webroot/js/facebook.js
+++ app/webroot/js/facebook.js
@@ -1,21 +1,23 @@
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
-    console.log(response);
 
     if (response.status === 'connected') {
-      console.log(response);
+      FB.api('/me', function(response) {
+        console.log(response);
+      
      /* var request = $.ajax({
-        url: "/login-by-facebook",
-        type: "POST",
-        data: {
-          name : response.name,
-        },
-        dataType: "html"
+          url: "/login-by-facebook",
+          type: "POST",
+          data: {
+            name : response.name,
+          },
+          dataType: "html"
+        });
+         
+        request.fail(function( jqXHR, textStatus ) {
+          alert( "Request failed: " + textStatus );
+        });*/
       });
-       
-      request.fail(function( jqXHR, textStatus ) {
-        alert( "Request failed: " + textStatus );
-      });*/
 
     } else if (response.status === 'not_authorized') {
       document.getElementById('status').innerHTML = 'Please log ' +
@@ -45,7 +47,6 @@
     });
   };
 
-  // Load the SDK asynchronously
   (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
@@ -53,14 +54,4 @@
     js.src = "//connect.facebook.net/fr_FR/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
+}
